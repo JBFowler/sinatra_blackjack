b81d87af588f164a5a15050e0504e5af8f80b494
Joe Fowler <jbfowler2@crimson.ua.edu>
2015-07-17 17:18:28 -0500

    Final course 3 web app files

--- main.rb
+++ main.rb
@@ -4,26 +4,184 @@ require 'pry'
 
 use Rack::Session::Cookie, :key => 'rack.session', :path => '/', :secret => 'sandy'
 
-# get '/' do
-#   erb :set_name
-# end
+BLACKJACK_AMOUNT = 21
+DEALER_MIN_HIT = 17
 
-# post '/set_name' do
-#   session[:player_name] = params[:player_name]
-#   redirect '/game'
-# end
+helpers do
+  def calculate_total(cards)
+    arr = cards.map{|element| element[1]}
 
-# get '/game' do
-#   session[:deck] = [['2', 'H'], ['3', 'D']]
-#   session[:player_cards] = []
-#   session[:player_cards] << session[:deck].pop
-#   erb :game
-# end
+    total = 0
+    arr.each do |a|
+      if a == 'A'
+        total += 11
+      else
+        total += a.to_i == 0 ? 10 : a.to_i
+      end
+    end
+      #correct for Aces
+    arr.select{|element| element == "A"}.count.times do
+      break if total <= BLACKJACK_AMOUNT
+      total -=10
+    end
 
-get '/home' do
+    total
+  end
+
+  def card_image(card)
+    suit = case card[0]
+      when 'H' then 'hearts'
+      when 'D' then 'diamonds'
+      when 'C' then 'clubs'
+      when 'S' then 'spades'
+    end
+
+    value = card[1]
+    if ['J', 'Q', 'K', 'A'].include?(value)
+      value = case card[1]
+        when 'J' then 'jack'
+        when 'Q' then 'queen'
+        when 'K' then 'king'
+        when 'A' then 'ace'  
+      end
+    end
+
+    "<img src='/images/cards/#{suit}_#{value}.jpg' class='card_image'>"
+  end
+
+  def winner!(msg)
+    @play_again = true
+    @success = "<strong>#{session[:player_name]} wins!</strong> #{msg}"
+    @show_hit_or_stay_button = false
+  end
+
+  def loser!(msg)
+    @play_again = true
+    @error = "<strong>#{session[:player_name]} loses.</strong> #{msg}"
+    @show_hit_or_stay_button = false
+  end
+
+  def tie!(msg)
+    @play_again = true
+    @success = "<strong>It's a tie!</strong> #{msg}"
+    @show_hit_or_stay_button = false
+  end
+end
+
+before do
+  @show_hit_or_stay_button = true
+end
+
+get '/' do
+  if session[:player_name]
+    redirect '/game'
+  else
+    redirect '/new_player'
+  end
+end
+
+get '/new_player' do
+  erb :set_name
+end
+
+post '/new_player' do
+  if params[:player_name].empty?
+    @error = "Name is required"
+    halt erb(:set_name)
+  end
+
+  session[:player_name] = params[:player_name]
+  redirect '/game'
+end
+
+get '/game' do
+  session[:turn] = session[:player_name]
+  #create a deck and put it into session
+  suits = ['H', 'D', 'C', 'S']
+  values = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+  session[:deck] = suits.product(values).shuffle!
+
+  # deal cards
+  session[:player_cards] = []
+  session[:dealer_cards] = []
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+  session[:dealer_cards] << session[:deck].pop
+  session[:player_cards] << session[:deck].pop
+
   erb :game
 end
 
-get '/nested_template' do
-  erb :"/Practice/example"
+post '/game/player/hit' do
+  session[:player_cards] << session[:deck].pop
+
+  player_total = calculate_total(session[:player_cards])
+  if player_total == BLACKJACK_AMOUNT
+    redirect '/game/dealer'
+    # winner!("#{session[:player_name]} hit blackjack!")
+  elsif player_total > BLACKJACK_AMOUNT
+    session[:turn] = "dealer"
+    loser!("It looks like #{session[:player_name]} busted at #{player_total}.")
+  end
+
+  erb :game
 end
+
+post '/game/player/stay' do
+  @success = "#{session[:player_name]} has chosen to stay"
+  @show_hit_or_stay_button = false
+  redirect '/game/dealer'
+end
+
+get '/game/dealer' do
+  session[:turn] = "dealer"
+  @show_hit_or_stay_button = false
+  dealer_total = calculate_total(session[:dealer_cards])
+
+  if dealer_total == BLACKJACK_AMOUNT
+    loser!("Dealer hit blackjack.")
+  elsif dealer_total > BLACKJACK_AMOUNT
+    winner!("Dealer busted at #{dealer_total}.")
+  elsif dealer_total >= DEALER_MIN_HIT
+    #dealer_stays
+    redirect '/game/compare'
+  else
+    #dealer_hits
+    @show_dealer_hit_button = true
+  end
+  erb :game
+end
+
+post '/game/dealer/hit' do
+  session[:dealer_cards] << session[:deck].pop
+  redirect '/game/dealer'
+end
+
+get '/game/compare' do
+  @show_hit_or_stay_button = false
+
+  player_total = calculate_total(session[:player_cards])
+  dealer_total = calculate_total(session[:dealer_cards])
+
+  if player_total < dealer_total
+    loser!("#{session[:player_name]} stayed at #{player_total}, and the dealer stayed at #{dealer_total}.")
+  elsif player_total == BLACKJACK_AMOUNT && player_total > dealer_total
+    winner!("#{session[:player_name]} hit backjack, and the dealer stayed at #{dealer_total}.")
+  elsif player_total > dealer_total
+    winner!("#{session[:player_name]} stayed at #{player_total}, and the dealer stayed at #{dealer_total}.")
+  elsif dealer_total == BLACKJACK_AMOUNT && player_total < dealer_total
+    loser!("#{session[:player_name]} stayed at #{player_total}, and the dealer hit Blackjack.")
+  else
+    tie!("Both #{session[:player_name]} and the dealer stayed at #{player_total}.")
+  end
+
+  erb :game
+end
+
+get '/game_over' do
+  erb :game_over
+end
+
+
+
+
